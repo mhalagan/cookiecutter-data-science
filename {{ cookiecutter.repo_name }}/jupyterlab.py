be6e59a59015f47e388470026bd458951b7aa817
Mike Halagan <>
2018-11-23 21:00:26 -0600

    Updated jupyterlab

--- {{ cookiecutter.repo_name }}/jupyterlab.py
+++ {{ cookiecutter.repo_name }}/jupyterlab.py
@@ -11,6 +11,15 @@ def post_save_hook(model, os_path, contents_manager):
     cwd, name = os.path.split(os_path)
 
     if model["type"] == "notebook" and ".ipynb" in os_path:
+        outdir = cwd + "/aspython"
+        if not os.path.isdir(outdir):
+            try:  
+                os.mkdir(outdir)
+            except OSError:  
+                print ("Creation of the directory %s failed" % outdir)
+            else:  
+                print ("Successfully created the directory %s " % outdir)
+
         output = "aspython/" + name.replace(".ipynb", ".py").lower()
 
         subprocess.check_call(
